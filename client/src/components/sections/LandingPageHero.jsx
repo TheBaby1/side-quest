8acbc5646181fd61bc73ad530790059078cf7413
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-14 01:44:12 +0800

    added bottom text in hero section

--- client/src/components/sections/LandingPageHero.jsx
+++ client/src/components/sections/LandingPageHero.jsx
@@ -1,12 +1,16 @@
 const LandingPageHero = () => {
     return(
         <>
-            <section className="w-full flex justify-center items-center py-20">
+            <section className="w-full flex-col justify-center items-center py-20">
                 <h1 className="text-6xl font-light text-center leading-tight">
                     <span className="block">Send Items. Get Items.</span>
                     <span>Earn your</span>
                     <span className="text-gray-400"> way.</span>
                 </h1>
+
+                <h3 className="font-light text-gray-400 text-center mt-4">
+                    Help deliver items to people and earn money along the way.
+                </h3>
             </section>
         </>
     );
