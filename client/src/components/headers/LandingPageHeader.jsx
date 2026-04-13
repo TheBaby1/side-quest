9a3f59d9dcb88d71a35f2a30f23626f863f095eb
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-13 22:29:51 +0800

    added landing page header

--- client/src/components/headers/LandingPageHeader.jsx
+++ client/src/components/headers/LandingPageHeader.jsx
@@ -1,9 +1,18 @@
 const LandingPageHeader = () => {
     return(
         <>
-            <div className="h-8 w-full bg-black">
-                <p className="text-red-100 text-2xl">hello world</p>
-            </div>
+            <header className="w-full bg-white px-6 py-4 flex flex-row justify-between">
+                <span className="text-black text-2xl font-bold">SideQuest</span>
+                <nav className="flex gap-6">
+                    <a href="#" className="text-black hover:text-gray-300">Home</a>
+                    <a href="#" className="text-black hover:text-gray-300">About Us</a>
+                    <a href="#" className="text-black hover:text-gray-300">Contact Us</a>
+                </nav>
+                <div className="flex gap-4">
+                    
+                </div>
+
+            </header>
         </>
     );
 }
