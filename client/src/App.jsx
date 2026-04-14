f4f6945df04a2c876de1f89c7621229567db2916
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-14 23:13:00 +0800

    added browser routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 
 function App() {
 
   return (
     <>
-      <LandingPage></LandingPage>
+      <Routes>
+        <Route path="/landing-page" element={<LandingPage></LandingPage>}/>
+
+      </Routes>
 
     </>
   )
