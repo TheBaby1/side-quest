4a29298de6df06a70f8dc346d828924b95c01198
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-15 01:39:25 +0800

    added cancel button component

---
+++ client/src/components/buttons/CancelButton.jsx
@@ -0,0 +1,13 @@
+const CancelButton = () => {
+    return (
+        <>
+            <button
+                className="bg-red-400 text-white px-4 py-2 rounded-full hover:bg-red-600 cursor pointer"
+            >
+                Cancel
+            </button>
+        </>
+    );
+}
+
+export default CancelButton;
