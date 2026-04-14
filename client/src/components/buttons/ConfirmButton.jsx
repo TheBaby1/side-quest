256987b4139d311bc9d1819c15da61da9881fee9
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-15 02:00:57 +0800

    added confirm button component

---
+++ client/src/components/buttons/ConfirmButton.jsx
@@ -0,0 +1,13 @@
+const ConfirmButton = () => {
+    return (
+        <>
+            <button
+                className="bg-yellow-400 text-black px-4 py-2 rounded-full hover:bg-yellow-600 cursor-pointer"
+            >
+                Confirm
+            </button>
+        </>
+    );
+}
+
+export default ConfirmButton;
