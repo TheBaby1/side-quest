f594d44c348326134a2191ca3393ad72cb46144a
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-16 23:36:55 +0800

    added error checking in sign up form fields

--- client/src/components/forms/SignUpForm.jsx
+++ client/src/components/forms/SignUpForm.jsx
@@ -22,7 +22,42 @@ const SignUpForm = () => {
         setErrors({...errors, [e.target.name]: ""});
     }
 
+    function validate() {
+        const newErrors = {}
+
+        if (!formData.username.trim()) {
+            newErrors.username = "Username is required."
+        }
+
+        if (!formData.email.trim()) {
+            newErrors.email = "Email is required."
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+            newErrors.email = "Enter a valid email.";
+        }
+
+        if (!formData.password) {
+            newErrors.password = "Password is required."
+        } else if (formData.password.length < 8) {
+            newErrors.password = "Password should be at least 8 characters"
+        }
+
+        if (!formData.confirmPassword) {
+            newErrors.confirmPassword = "Please confirm your password."
+        } else if (formData.confirmPassword !== formData.password) {
+            newErrors.confirmPassword = "Passwrods do not match."
+        }
+
+        return newErrors;
+    }
+
     function handleSubmit() {
+        const newErrors = validate();
+
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return; 
+        }
+
         console.log(formData);
     }
 
@@ -37,6 +72,7 @@ const SignUpForm = () => {
                     placeholder="Username"
                     className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"
                 />
+                {errors.username && <p className="text-red-500 text-xs">{errors.username}</p>}
 
                 <input
                     type="email"
@@ -46,6 +82,7 @@ const SignUpForm = () => {
                     placeholder="Email"
                     className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"
                 />
+                {errors.email && <p className="text-red-500 text-xs">{errors.email}</p>}
 
                 <input
                     type="password"
@@ -55,6 +92,7 @@ const SignUpForm = () => {
                     placeholder="Password"
                     className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"
                 />
+                {errors.password && <p className="text-red-500 text-xs">{errors.password}</p>}
 
                 <input
                     type="password"
@@ -64,6 +102,7 @@ const SignUpForm = () => {
                     placeholder="Confirm Password"
                     className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black mb-4"
                 />
+                {errors.confirmPassword && <p className="text-red-500 text-xs">{errors.confirmPassword}</p>}
 
                 <BigBlackButton
                     label="Create My Account"
