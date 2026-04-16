5ea7920dfba4829d4f0783ff993b4a8af679c888
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-17 00:08:25 +0800

    added closing functionality when clicking outisde of modal

--- client/src/components/modals/SignUpModal.jsx
+++ client/src/components/modals/SignUpModal.jsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from 'react'
+
 import CloseButton from "../buttons/CloseButton";
 import CancelButton from "../buttons/CancelButton";
 import ConfirmButton from "../buttons/ConfirmButton";
@@ -6,10 +8,26 @@ import SignUpForm from "../forms/SignUpForm";
 const SignUpModal = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
 
+    const modalRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(e) {
+            if (modalRef.current && !modalRef.current.contains(e.target)) {
+                onClose();
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousdeown", handleClickOutside);
+        }
+    }, [onClose]);
+
     return(
         <>
             <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
-                <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6">
+                <div ref={modalRef} className="bg-white rounded-lg shadow-lg w-full max-w-md p-6">
                     <div className="flex flex-col justify-center items-center mb-4">
                         <h2 className="mb-4">Logo here</h2>
                         <h2 className="text-black text-2xl font-semibold mb-4">Join Our Exclusive Community!</h2>
