a603415f23d3d6680c825dd17433fb541963d2c7
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-17 23:09:28 +0800

    completed login modal skeleton

--- client/src/components/modals/LoginModal.jsx
+++ client/src/components/modals/LoginModal.jsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect } from 'react'
 
 import LoginForm from '../forms/LoginForm';
+import OtherOptionButton from '../buttons/sign-up-buttons/OtherOptionButton';
 
 const LoginModal = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
@@ -25,14 +26,35 @@ const LoginModal = ({ isOpen, onClose }) => {
         <>
             <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50">
                 <div ref={modalRef} className="bg-white rounded-lg shadow-lg w-full max-w-md p-6">
-                    <div className="flex justify-center items-center mb-4">
+                    <div className="flex flex-col justify-center items-center mb-4">
                         <h2 className="text-black text-2xl font-semibold mb-4">Welcome back!</h2>
-
+                        <p className="text-gray-400 text-sm text-center px-6">
+                            Come back in and go right where you left!
+                        </p>
                     </div>
 
                     <LoginForm
                         onSuccess={onClose}
                     />
+
+                    <p className="text-gray-400 text-sm text-center px-6 mt-4">
+                        Or continue with
+                    </p>
+
+                    <div className="flex justify-center items-center gap-1 mt-4">
+                        <OtherOptionButton
+                            label="Facebook"
+                            onClick="" // TODO
+                        />
+                        <OtherOptionButton
+                            label="Google"
+                            onClick="" // TODO
+                        />
+                        <OtherOptionButton
+                            label="Apple"
+                            onClick="" // TODO
+                        />
+                    </div>
                 </div>
             </div>
         </>
