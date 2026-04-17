691cf49724f5f746d13e33d33c9c3369b26c898f
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-17 23:42:32 +0800

    chore: initialized server-side

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+
+// server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
