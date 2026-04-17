f28f8e7d5d6ce72aefeb702088e82550bd1ed3e6
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-18 00:40:15 +0800

    chore: connected to mongodb

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -12,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // routes
-app.use("/api/auth", authRoutes);
+// app.use("/api/auth", authRoutes);
 
 // server
 const PORT = process.env.PORT || 5000;
