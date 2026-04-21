bb2566919c2016aa6b576220bb296efcf73a6c0f
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-21 23:40:47 +0800

    fix: minor issue

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 // server
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
