4ecc900723705afc202ff7d15f026df07e4235dc
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-24 23:12:25 +0800

    feat: added authentication middleware

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createTask } from '../controllers/taskController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/new-task', createTask);
+router.post('/new-task', protect, createTask);
 
 export default router;
