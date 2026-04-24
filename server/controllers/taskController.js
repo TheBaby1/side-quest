299502473ebcebac460a722021e475efd308df3c
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-24 22:36:36 +0800

    feat: added get task route

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,4 +1,5 @@
 import Task from '../models/Task.js';
+import User from '../models/User.js';
 
 export const createTask = async (req, res) => {
     try {
@@ -15,7 +16,22 @@ export const createTask = async (req, res) => {
             res.status(400).json({ message: 'Task was not created successfully.' });
         }
 
-        res.status(200).json(newTask);
+        res.status(201).json(newTask);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error.', error: error.message });
+    }
+}
+
+export const displayUserTasks = async (req, res) => {
+    try {
+        const existingUser = await User.findOne({ _id: req.user.id });
+        if (!existingUser) {
+            res.status(400).json({ message: 'User does not exist.' });
+        }
+
+        const tasks = await Task.find({ userId: req.user.id });
+
+        res.satus(200).json(tasks);
     } catch (error) {
         res.status(500).json({ message: 'Server error.', error: error.message });
     }
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTask } from '../controllers/taskController';
+import { createTask } from '../controllers/taskController.js';
 
 const router = express.Router();
 
