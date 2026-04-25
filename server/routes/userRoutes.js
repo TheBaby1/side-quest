d22994505f359d0a8f8b694044d5a58d8c7c9b5b
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-25 22:45:58 +0800

    feat: added update user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { getAllUsers, getUserByEmail, getUserByUsername } from '../controllers/userController.js';
+import { 
+    getAllUsers, 
+    getUserByEmail, 
+    getUserByUsername,
+    updateUserById
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/get-all-users', getAllUsers);
 router.post('/get-by-username', getUserByUsername);
 router.post('/get-by-email', getUserByEmail);
+router.put('/update-by-id', updateUserById);
 
 export default router;
