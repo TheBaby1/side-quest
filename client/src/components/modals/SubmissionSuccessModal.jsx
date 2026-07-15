666ee8d48bc5494e96ab38a90c6f7babc2e70580
Nikko Ensomo <nikkonuriel@gmail.com>
2026-07-15 15:12:03 +0800

    refactor: made changes to submission success modal

--- client/src/components/modals/SubmissionSuccessModal.jsx
+++ client/src/components/modals/SubmissionSuccessModal.jsx
@@ -6,8 +6,13 @@ const SubmissionSuccessModal = ({ onClick, isDisabled }) => {
             <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50 px-4">
                 <div className="w-full max-w-md bg-white rounded-lg shadow-lg px-4">
                     <div className="flex flex-col gap-4">
-                        <span>hello</span>
-                        <BigBlackButton 
+                        <span className="text-zinc-950 font-semibold text-lg">Message sent!</span>
+                        <span className="text-sm font-medium text-gray-700"
+                        >
+                            Thanks for reaching out. We'll get back to you shortly.
+                        </span>
+                        
+                        <BigBlackButton
                             label='Close'
                             onClick={onClick}
                             isDisabled={isDisabled}
